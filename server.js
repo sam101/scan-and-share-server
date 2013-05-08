f9ac1f6aa0dd6196c8885aff1cbf4e56e773556e
Nathan <nathan@nathan-laptop.home>
2013-05-08 18:11:45 +0200

    Express skeleton

---
+++ server.js
@@ -0,0 +1,71 @@
+/************************************/
+/*			REQUIRES				*/
+/************************************/
+var express = require('express');
+var login = require('./login.js');
+var ean = require('./ean.js');
+var register = require('./register.js');
+var sales = require('./sales.js');
+
+/************************************/
+/*			PARAMETERS				*/
+/************************************/
+var port = process.argv[2];
+
+
+/************************************/
+/*			REST INTERFACE			*/
+/************************************/
+var app = express();
+app.use(express.bodyParser());	// Parse automatically a POST request body in JSON
+
+
+		/*		GET 	*/
+
+// LOGIN request
+app.get('/login', function (req, res)
+{
+	res.send();
+});
+
+// PRODUCT request
+app.get('/ean', function (req, res)
+{
+	console.log(req.query);
+	res.send();
+});
+
+// SALES request
+app.get('/sales', function (req, res)
+{
+	console.log(req.query);
+	res.send();
+});
+
+		/*		POST 	*/
+
+// REGISTER request
+app.post('/register', function (req, res)
+{
+	console.log(req.body);
+	res.send();
+});
+
+// PRODUCT REQUEST
+app.post('/ean', function (req, res)
+{
+	console.log(req.query);
+	console.log(req.body);
+	res.send();
+});
+
+// PRODUCT REQUEST
+app.post('/sales', function (req, res)
+{
+	console.log(req.query);
+	console.log(req.body);
+	res.send();
+});
+
+app.listen(port);
+
